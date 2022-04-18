a4d3482a8eeb285a2eb821caccacf90bd2870e58
hkaneko1985 <hkaneko@meiji.ac.jp>
2022-04-18 21:05:30 +0900

    version up

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE.txt') as f:
 
 setup(
     name='dcekit',
-    version='2.6.19',
+    version='2.6.20',
     description='Data Chemical Engineering toolkit',
     long_description=readme,
     author='Hiromasa Kaneko',
